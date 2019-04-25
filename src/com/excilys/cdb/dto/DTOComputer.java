e33a64ae840bca3760ea325524b3322dfb9a45d5
marezes <marezes.mikearezes@msn.com>
2019-04-25 15:43:30 +0200

    Ajout des méthodes hash, equals et toString aux 3 objet DTO

--- src/com/excilys/cdb/dto/DTOComputer.java
+++ src/com/excilys/cdb/dto/DTOComputer.java
@@ -3,24 +3,71 @@ package com.excilys.cdb.dto;
 public class DTOComputer {
 	private String id; // un id
 	private String name; // Nom de l'ordinateur
-	private String di; // date introduced
-	private String dd; // date discontinued
+	private String introduced; // date introduced
+	private String discontinued; // date discontinued
 	private String manufacturer; // le fabricant
-
-	public DTOComputer() {
-		this.id = null;
-		this.name = null;
-		this.di = null;
-		this.dd = null;
-		this.manufacturer = null;
-	}
 	
-	public DTOComputer(String id, String name, String di, String dd, String manufacturer) {
+	public DTOComputer(String id, String name, String introduced, String discontinued, String manufacturer) {
 		this.setId(id);
 		this.setName(name);
-		this.setDi(di);
-		this.setDd(dd);
+		this.setIntroduced(introduced);
+		this.setDiscontinued(discontinued);
 		this.setManufacturer(manufacturer);
+	}
+
+	@Override
+	public String toString() {
+		return "DTOComputer [id=" + id + ", name=" + name + ", introduced=" + introduced + ", discontinued="
+				+ discontinued + ", manufacturer=" + manufacturer + "]";
+	}
+
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((discontinued == null) ? 0 : discontinued.hashCode());
+		result = prime * result + ((id == null) ? 0 : id.hashCode());
+		result = prime * result + ((introduced == null) ? 0 : introduced.hashCode());
+		result = prime * result + ((manufacturer == null) ? 0 : manufacturer.hashCode());
+		result = prime * result + ((name == null) ? 0 : name.hashCode());
+		return result;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		DTOComputer other = (DTOComputer) obj;
+		if (discontinued == null) {
+			if (other.discontinued != null)
+				return false;
+		} else if (!discontinued.equals(other.discontinued))
+			return false;
+		if (id == null) {
+			if (other.id != null)
+				return false;
+		} else if (!id.equals(other.id))
+			return false;
+		if (introduced == null) {
+			if (other.introduced != null)
+				return false;
+		} else if (!introduced.equals(other.introduced))
+			return false;
+		if (manufacturer == null) {
+			if (other.manufacturer != null)
+				return false;
+		} else if (!manufacturer.equals(other.manufacturer))
+			return false;
+		if (name == null) {
+			if (other.name != null)
+				return false;
+		} else if (!name.equals(other.name))
+			return false;
+		return true;
 	}
 
 	// Getters
@@ -33,12 +80,12 @@ public class DTOComputer {
 		return name;
 	}
 
-	public String getDi() {
-		return di;
+	public String getIntroduced() {
+		return introduced;
 	}
 
-	public String getDd() {
-		return dd;
+	public String getDiscontinued() {
+		return discontinued;
 	}
 
 	public String getManufacturer() {
@@ -55,12 +102,12 @@ public class DTOComputer {
 		this.name = name;
 	}
 
-	public void setDi(String di) {
-		this.di = di;
+	public void setIntroduced(String introduced) {
+		this.introduced = introduced;
 	}
 
-	public void setDd(String dd) {
-		this.dd = dd;
+	public void setDiscontinued(String discontinued) {
+		this.discontinued = discontinued;
 	}
 
 	public void setManufacturer(String manufacturer) {
