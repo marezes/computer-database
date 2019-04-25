ca2aed72bfd1220a9af84b3f88e1b747ab5436dc
marezes <marezes.mikearezes@msn.com>
2019-04-25 16:14:51 +0200

    Transformation de ServiceCompany en Singleton et ajout de la méthode requestList

--- src/com/excilys/cdb/service/ServiceCompany.java
+++ src/com/excilys/cdb/service/ServiceCompany.java
@@ -1,14 +1,34 @@
 package com.excilys.cdb.service;
 
+import java.util.ArrayList;
+
+import com.excilys.cdb.dto.DTOCompany;
 import com.excilys.cdb.mapper.MapperCompany;
-import com.excilys.cdb.model.ModelCompany;
+import com.excilys.cdb.persistence.DAOCompany;
 
 public class ServiceCompany {
-	MapperCompany mapperCompany;
-	ModelCompany modelCompany;
+	private static final ServiceCompany INSTANCE = new ServiceCompany();
+	private MapperCompany mapperCompany;
+	private DAOCompany daoCompany;
 	
-	public ServiceCompany() {
-		this.mapperCompany = new MapperCompany();
-		modelCompany = null;
+	private ServiceCompany() {
+		mapperCompany = MapperCompany.getInstance();
+		daoCompany = DAOCompany.getInstance();
+	}
+	
+	/**
+	 * Méthode qui renvoie l'objet singleton ServiceCompany.
+	 * @return Un objet de type ServiceCompany
+	 */
+	public static ServiceCompany getInstance() {
+		return INSTANCE;
+	}
+	
+	/**
+	 * Méthode qui récupère la liste des entreprises.
+	 * @return Une ArrayList de DTOCompany
+	 */
+	public ArrayList<DTOCompany> requestList() {
+		return mapperCompany.modelCompanyListToDTOCompanyList(daoCompany.requestList());
 	}
 }
