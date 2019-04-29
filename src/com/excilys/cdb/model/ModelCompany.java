f0a2247aa5b2264300ea6e385fed6c277da4e518
marezes <marezes.mikearezes@msn.com>
2019-04-29 11:00:17 +0200

    Ajout de hashCode et equals à ModelCompany

--- src/com/excilys/cdb/model/ModelCompany.java
+++ src/com/excilys/cdb/model/ModelCompany.java
@@ -11,7 +11,35 @@ public class ModelCompany {
 	
 	@Override
 	public String toString() {
-		return ("[id = " + id + ", nom de l'entreprise = " + name + "]");
+		return "ModelCompany [id=" + id + ", name=" + name + "]";
+	}
+
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + id;
+		result = prime * result + ((name == null) ? 0 : name.hashCode());
+		return result;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		ModelCompany other = (ModelCompany) obj;
+		if (id != other.id)
+			return false;
+		if (name == null) {
+			if (other.name != null)
+				return false;
+		} else if (!name.equals(other.name))
+			return false;
+		return true;
 	}
 	
 	// Getters
@@ -33,6 +61,4 @@ public class ModelCompany {
 	public void setName(String name) {
 		this.name = name;
 	}
-	
-	
 }
