a37f3bc566ea65f8b1d45b2f1c32bc05c11f8c0e
marezes <marezes.mikearezes@msn.com>
2019-04-29 17:38:12 +0200

    Changement du Main

--- src/com/excilys/cdb/main/Main.java
+++ src/com/excilys/cdb/main/Main.java
@@ -1,14 +1,10 @@
 package com.excilys.cdb.main;
 
-import com.excilys.cdb.persistence.DAOComputer;
 import com.excilys.cdb.ui.*;
 
 public class Main {
 	public static void main(String[] args) {
 		UI ui = new UI();
-		ui.init();
-		//ui.start();
-		DAOComputer test = DAOComputer.getInstance();
-		test.requestList();
+		ui.start();
 	}
 }
