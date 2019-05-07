f8f47a10869a8f1e1752ae93fc8f9119bfc3bb26
marezes <marezes.mikearezes@msn.com>
2019-05-07 18:28:09 +0200

    Ajout d'un test Junit

--- src/test/java/mapper/MapperCompanyTest.java
+++ src/test/java/mapper/MapperCompanyTest.java
@@ -2,13 +2,24 @@ package mapper;
 
 import static org.junit.Assert.*;
 
+import java.util.ArrayList;
+
 import org.junit.Test;
+
+import com.excilys.cdb.dto.DTOCompany;
+import com.excilys.cdb.mapper.MapperCompany;
+import com.excilys.cdb.model.ModelCompany;
 
 public class MapperCompanyTest {
 
 	@Test
-	public void test() {
-		assertTrue(true);
+	public void testModelCompanyListToDTOCompanyList() {
+		ArrayList<ModelCompany> modelCompanyList = new ArrayList<ModelCompany>();
+		modelCompanyList.add(new ModelCompany(1, "Bidon"));
+		ArrayList<DTOCompany> dtoCompanyList = MapperCompany.getInstance().modelCompanyListToDTOCompanyList(modelCompanyList);
+		
+		assertEquals("1", dtoCompanyList.get(0).getId());
+		assertEquals("Bidon", dtoCompanyList.get(0).getName());
 	}
 
 }
