c66d42d72057a2a0ad8bcef4b4b0b30701e15885
marezes <marezes.mikearezes@msn.com>
2019-05-27 17:59:30 +0200

    Changement d'une requête en JDBCTemplate sur DAOCompany

--- src/main/java/com/excilys/cdb/persistence/DAOCompany.java
+++ src/main/java/com/excilys/cdb/persistence/DAOCompany.java
@@ -4,9 +4,10 @@ import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
-import java.sql.Statement;
 import java.util.ArrayList;
 
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.core.RowMapper;
 import org.springframework.stereotype.Repository;
 
 import com.excilys.cdb.dbConfig.Hikari;
@@ -32,39 +33,26 @@ public class DAOCompany {
 	 * @throws SQLException 
 	 */
 	public ArrayList<ModelCompany> requestList() throws SQLException {
-		ArrayList<ModelCompany> model = new ArrayList<ModelCompany>();
+		ArrayList<ModelCompany> modelCompanyList;
 		
-		try (Connection connexion = dataSource.getConnection()) {
-			
-			/* Création de l'objet gérant les requêtes */
-			try (Statement statement = connexion.createStatement()) {
+		JdbcTemplate jdbcTemplate = new JdbcTemplate(dataSource);
+		
+		RowMapper<ModelCompany> rowMapper = new RowMapper<ModelCompany>() {
+			public ModelCompany mapRow(ResultSet resultat, int rowNumber) throws SQLException {
+				int id = resultat.getInt("id");
+				String name = resultat.getString("name");
 				
-				/* Exécution d'une requête de lecture */
-				try (ResultSet resultat = statement.executeQuery(SELECT)) {
-					while (resultat.next()) {
-						int id = resultat.getInt("id");
-						String name = resultat.getString("name");
-						model.add(new ModelCompany.ModelCompanyBuilder()
-								.withId(id)
-								.withName(name)
-								.build());
-					}
-				} catch (SQLException e) {
-					// System.err.println("Récupération de la requête raté.");
-					throw e;
-				}
-				
-			} catch (SQLException e) {
-				// System.err.println("Création du statement raté.");
-				throw e;
+				ModelCompany modelCompany = new ModelCompany.ModelCompanyBuilder()
+						.withId(id)
+						.withName(name)
+						.build();
+				return modelCompany;
 			}
+		};
+		
+		modelCompanyList = (ArrayList<ModelCompany>) jdbcTemplate.query(SELECT, rowMapper);
 
-		} catch (SQLException e) {
-			// System.err.println("Problème dans la connexion à la base SQL");
-			throw e;
-		}
-
-		return model;
+		return modelCompanyList;
 	}
 	
 	public boolean requestDelete(int id) throws RuntimeException, Exception {
